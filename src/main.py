1857932390e892c094375e7ee3d3e512582c70ad
John Philip <johnphilipomamalin1206@gmail.com>
2025-05-09 16:20:15 +0800

    cleaning main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routers import contacts  # import your routers here
-# from src.utils.logger import setup_logfire
+from src.routers import contacts
+from src.utils.logger import setup_logfire
 
 web_app = FastAPI()
 
-# setup_logfire(web_app)
-
-# Set up CORS with your allowed origins
-# origins = [
-#     "http://localhost:3000",
-#     "http://10.73.130.177:3000",
-#     "http://10.74.12.26:3000",
-#     "http://172.24.68.121:3000",
-#     "https://nextjs-fastapi-supa.vercel.app" # THIS WILL BE YOUR DOMAIN ON YOUR FRONTEND
-# ]
+setup_logfire(web_app)
 
 web_app.add_middleware(
     CORSMiddleware,
